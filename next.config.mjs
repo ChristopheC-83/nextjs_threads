264fcfd9df4c96502d7593a0877143290b8817c6
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-26 21:55:24 +0100

    inscription mongodb

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { PHRSE_DEVELOPMENT_SERVER } = require("next/constants");
 
-export default nextConfig;
+module.exports = (env) => {
+  if ((env = PHRSE_DEVELOPMENT_SERVER)) {
+    return {
+      env: {
+        MONGODB_CLIENT:
+          "mongodb+srv://kikisan:apqOuH4V8cCkH1ZH@cluster0.qh54wye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      },
+    };
+  } else {
+    return {
+      env: {
+        MONGODB_CLIENT:
+          "mongodb+srv://kikisan:apqOuH4V8cCkH1ZH@cluster0.qh54wye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      },
+    };
+  }
+};
