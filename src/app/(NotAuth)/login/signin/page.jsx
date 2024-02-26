9a464122823c5682088a6693eb7718b52cecd5fd
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-26 21:17:22 +0100

    form ok

--- src/app/(NotAuth)/login/signin/page.jsx
+++ src/app/(NotAuth)/login/signin/page.jsx
@@ -1,44 +1,25 @@
 /* eslint-disable react/no-unescaped-entities */
-// page d'enregistrement
-"use client";
+// page de connexion
 
-import { createUser } from "@/actions/createUser";
+"use client";
 import Button from "@/components/Button/Button";
 import Link from "next/link";
 import { PiArrowLeftLight } from "react-icons/pi";
 
-export default function SignUp() {
-  async function prepareCreateUser(formData) {
-    const username = formData.get("username");
-    const pseudo = formData.get("pseudo");
+export default function SignIn() {
+  async function prepareLogin(formData) {
     const email = formData.get("email");
     const password = formData.get("password");
-
-    console.log(username, pseudo, email, password);
-
-    await createUser(username, pseudo, email, password);
+    console.log(email, password);
   }
-
   return (
     <div className="mx-auto w-[500px] flex flex-col gap-4">
       <Link className="flex items-center" href="/login">
         <PiArrowLeftLight className="mr-2 text-2xl text-white" />
-        <h1 className="title">Inscrivez-vous!</h1>
+        <h1 className="title">Connectez-vous!</h1>
       </Link>
 
-      <form action={prepareCreateUser}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Nom d'utilisateur"
-          className="input"
-        />
-        <input
-          type="text"
-          name="pseudo"
-          placeholder="Pseudo de l'utilisateur"
-          className="input"
-        />
+      <form action={prepareLogin}>
         <input
           type="email"
           name="email"
@@ -52,7 +33,7 @@ export default function SignUp() {
           className="input"
         />
         <Button>
-          <p className="text-xl font-bold">S'inscrire</p>
+          <p className="text-xl font-bold">Connexion</p>
         </Button>
       </form>
       <div className="flex items-center justify-center mt-4">
@@ -60,8 +41,8 @@ export default function SignUp() {
         <div className="mx-4 mb-1 text-threads-gray-light">ou</div>
         <div className="w-1/4 border-t border-threads-gray-light"></div>
       </div>
-      <Link href="/login/signin" className="text-xl font-bold">
-        <Button>Se connecter</Button>
+      <Link href="/login/signup" className="text-xl font-bold">
+        <Button>Créer un compte</Button>
       </Link>
     </div>
   );
